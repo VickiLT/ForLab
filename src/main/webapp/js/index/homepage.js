e77bd41d24968777eb89a0926315f8cdf119a1db
luer <1242101824@qq.com>
2018-03-22 19:47:45 +0800

    导航栏响应bug修改

--- src/main/webapp/js/index/homepage.js
+++ src/main/webapp/js/index/homepage.js
@@ -6,45 +6,45 @@ $(function() {
             {
                 title: '机房巡检',
                 children: [
-                    {title: '设备状态检测', url: '/luer/jsp/labInspect/labInspect.jsp',isSelect:false},
-                    {title: '机房环境检测#', url: '/luer/jsp/labInspect/environmentIns.jsp',isSelect:false},
-                    {title: '机房异物检测', url: '/luer/jsp/infoProcess/InfoProcess.jsp',isSelect:false},
-                    {title: '机房异常声音检测', url: '/luer/jsp/reportForm/reportForm.jsp',isSelect:false}
+                    {title: '设备状态检测', url: '/luer/jsp/labInspect/labInspect.jsp'},
+                    {title: '机房环境检测#', url: '/luer/jsp/labInspect/environmentIns.jsp'},
+                    {title: '机房异物检测', url: '/luer/jsp/infoProcess/InfoProcess.jsp'},
+                    {title: '机房异常声音检测', url: '/luer/jsp/reportForm/reportForm.jsp'}
                 ]
             },
             {
                 title: '巡检方案管理',
                 children: [
-                    {title: '日常例行路线#', url: '/luer/jsp/InspectManage/routManage.jsp',isSelect:false},
-                    {title: '调度式', url: '/luer/jsp/labInspect/labInspect.jsp',isSelect:false},
-                    {title: '自主巡检', url: '/luer/jsp/InspectManage/inspectManage.jsp',isSelect:false},
-                    {title: '机房施工管理', url: '/luer/jsp/infoProcess/InfoProcess.jsp',isSelect:false},
-                    {title: '机器人自我维护', url: '/luer/jsp/reportForm/reportForm.jsp',isSelect:false}
+                    {title: '日常例行路线#', url: '/luer/jsp/InspectManage/routManage.jsp'},
+                    {title: '调度式', url: '/luer/jsp/labInspect/labInspect.jsp'},
+                    {title: '自主巡检', url: '/luer/jsp/InspectManage/inspectManage.jsp'},
+                    {title: '机房施工管理', url: '/luer/jsp/infoProcess/InfoProcess.jsp'},
+                    {title: '机器人自我维护', url: '/luer/jsp/reportForm/reportForm.jsp'}
                 ]
             },
             {
                 title: '巡检信息处理',
                 children: [
-                    {title: '音、视频信息', url: '/luer/jsp/indexContent.jsp',isSelect:false},
-                    {title: '设备状态分析及告警#', url: '/luer/jsp/infoProcess/alarmProcess.jsp',isSelect:false},
-                    {title: '设备故障预判', url: '/luer/jsp/InspectManage/inspectManage.jsp',isSelect:false},
-                    {title: '巡检结果生成及发布', url: '/luer/jsp/infoProcess/InfoProcess.jsp',isSelect:false}
+                    {title: '音、视频信息', url: '/luer/jsp/indexContent.jsp'},
+                    {title: '设备状态分析及告警#', url: '/luer/jsp/infoProcess/alarmProcess.jsp'},
+                    {title: '设备故障预判', url: '/luer/jsp/InspectManage/inspectManage.jsp'},
+                    {title: '巡检结果生成及发布', url: '/luer/jsp/infoProcess/InfoProcess.jsp'}
                 ]
             },
             {
                 title: '报表统计管理',
                 children: [
-                    {title: '巡检报表#', url: '/luer/jsp/reportForm/inspectRep.jsp',isSelect:false},
-                    {title: '故障统计', url: '/luer/jsp/labInspect/labInspect.jsp',isSelect:false},
-                    {title: '网络可用性', url: '/luer/jsp/InspectManage/inspectManage.jsp',isSelect:false},
-                    {title: '自定义报表', url: '/luer/jsp/infoProcess/InfoProcess.jsp',isSelect:false}
+                    {title: '巡检报表#', url: '/luer/jsp/reportForm/inspectRep.jsp'},
+                    {title: '故障统计', url: '/luer/jsp/labInspect/labInspect.jsp'},
+                    {title: '网络可用性', url: '/luer/jsp/InspectManage/inspectManage.jsp'},
+                    {title: '自定义报表', url: '/luer/jsp/infoProcess/InfoProcess.jsp'}
                 ]
             },
             {
                 title: '电子地图',
                 children: [
-                    {title: '二维电子地图#', url: '/luer/jsp/electMap/DimenElectMap.jsp',isSelect:false},
-                    {title: '实时显示位置', url: '/luer/jsp/labInspect/labInspect.jsp',isSelect:false}
+                    {title: '二维电子地图#', url: '/luer/jsp/electMap/DimenElectMap.jsp'},
+                    {title: '实时显示位置', url: '/luer/jsp/labInspect/labInspect.jsp'}
                 ]
             }
         ];
@@ -55,14 +55,21 @@ $(function() {
         },
         data: function () {
             return {
-                open: false
+                selected: "主页"
             }
         },
         methods: {
-            selectNav: function(isSelect,event){
-                if(isSelect){ return; }
-                this.model.isSelect=true;
+            selectNav: function(event){
                 var selectedNav=event.currentTarget;
+                if($(selectedNav).hasClass("active")){
+                    return;
+                }
+                var homeNavKs=$(".homeNavCLi");
+                for(var i=0;i<homeNavKs.length;i++){
+                    $(homeNavKs[i]).removeClass("active")
+                }
+
+                $(selectedNav).addClass("active");
                 var ContenSrc=selectedNav.getAttribute("data-url");
                 var iframeS="<iframe src="+ContenSrc+" ></iframe>";
                 $("iframe").remove();
@@ -76,7 +83,6 @@ $(function() {
         data: {
             treeData: homeNavData,
             open:"主页",
-            isSelect:"主页"
         },
         methods:{
             toggle: function (title,event) {
